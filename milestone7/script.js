78f7b40af68c0820247fca0cb65546c3936cd444
Linda <linmestra.3345@gmail.com>
2026-08-07 18:30:36 -0600

    script.js fixing if and section names

--- milestone7/script.js
+++ milestone7/script.js
@@ -1,12 +1,16 @@
 const hambrgBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
+if (hambrgBtn && nav) {
+    hambrgBtn.addEventListener('click', function() {
+        nav.classList.toggle('nav-open');
+    });
+}
 
-hambrgBtn.addEventListener('click', function() {
-    nav.classList.toggle('nav-open');
-});
-
-document.querySelector('.signUp-section form').addEventListener('submit', function(event){
-    event.preventDefault()
-    document.querySelector(".form-confirmation").classList.add("show");
-    form.reset();
-})
+const signupForm = document.querySelector('.signUp-section form');
+if (signupForm) {
+    signupForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        document.querySelector(".form-confirmation").classList.add("show");
+        signupForm.reset();
+    });
+}
